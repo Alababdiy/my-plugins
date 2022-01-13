7942968442b53e9168dbe6dbec75105ee9dafe41
abdallah <30894733+Alababdiy@users.noreply.github.com>
2022-01-13 10:17:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ app.secret = os.environ.get('534o5o34o5jpo234oomdfsog')
 
 
 #ok
-
+#Abdullah
 #wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
 @app.route('/send_phone')
 def to_contact():
